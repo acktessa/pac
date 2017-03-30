3f0383d0f40f751f112606b5a4635d8840d6462a
Tessa Kenfack Arnaud-cedric <tessakenfack@a10p14.fil.univ-lille1.fr>
2017-03-30 15:14:11 +0200

    ajout des nouveaux fichier, reunification

--- tools.py
+++ tools.py
@@ -1,12 +1,15 @@
 import sys
 
 from client import Connection
+from openssl import *
 
 class Helper:
 
     def __init__(self):
         self.connection = Connection("http://pac.fil.cool/uglix")
         self.itsTarget = None
+        self.user = 'tunderwood'
+        self.password = 'x+gC8HobK%'
         
     def connectionTest(self):
         print(self.connection.get("/"))
@@ -17,30 +20,69 @@ class Helper:
             self.itsTarget = target
             
         elif target == "tunderwood":
-            print(self.connection.post('/bin/login', user = "tunderwood", password = "x+gC8HobK%"))    
+            print(self.connection.post('/bin/login', user = 'tunderwood', password = 'x+gC8HobK%'))    
             self.itsTarget = target
 
         else:
             print("Unknown target. Check your username.\n")
 
+    def chapGetChallenge(self):
+        data = self.connection.get("/bin/login/CHAP")
+        return (data['challenge'])
+
+    def chapGetPlainText(self):
+        username = self.user
+        challenge = self.chapGetChallenge()
+        plainText = username + '-' + challenge
+        return (plainText)
+
+    def chapBuildChallengeResponse(self):
+        plainText = self.chapGetPlainText()
+        cryptedPlainText = encrypt(plainText, self.password)
+        return (cryptedPlainText)
+
+    def chapConnect(self):
+        challengeResponse = self.chapBuildChallengeResponse()
+        print(self.connection.post("/bin/login/CHAP",
+                                   user = self.user,
+                                   response = challengeResponse))
+        self.itsTarget = self.user
+
+    def souleymaneConnect(self):
+        challenge = self.connection.get('/bin/login/CHAP')
+        return self.connection.post('/bin/login/CHAP', user=self.user, response=encrypt(self.user + '-' + challenge['challenge'], self.password))
+        
     def echo(self):
         kwds = {'name':'toto', 'age':'42'}
         print(self.connection.post('/bin/echo', **kwds))
-            
+
+    def fileGetContent(self, filename):
+        file = open(filename, 'r')
+        content = file.read()
+        file.close()
+        return (content)
+        
     def mailList(self):
         print(self.connection.get("/home/" + self.itsTarget + "/INBOX"))
 
     def mailDisplay(self, number):
         print(self.connection.get("/home/" + self.itsTarget + "/INBOX/" + number))
 
-    def mailDisplayBody(self, number):
+    def mailBodyDisplay(self, number):
         print(self.connection.get("/home/" + self.itsTarget + "/INBOX/" + number + "/body"))
 
+    def mailBodyGet(self, number):
+        return self.connection.get("/home/" + self.itsTarget + "/INBOX/" + number + "/body")
+
+    def mailBodySave(self, number):
+        body = self.connection.get("/home/" + self.itsTarget + "/INBOX/" + number + "/body")
+        file = open('mail-body', 'w')
+        file.write(body)
+        file.close()
+        
     def mailSend(self, recipient, subjectText, contentText):
-        print(self.connection.post('/bin/sendmail',
-                                   to = recipient,
-                                   subject = subjectText,
-                                   content = contentText))
+        kwds = {'to': recipient, 'subject': subjectText, 'content': contentText}
+        print(self.connection.post('/bin/sendmail', **kwds))
 
     def home(self):
         print(self.connection.get("/home/"))
@@ -56,6 +98,14 @@ class Helper:
                                   + number
                                   + "/attachment/" + filename))
 
+    def ticketDownloadAttachment(self, number, filename):
+        file = open(filename, 'w')
+        content = self.connection.get("/bin/crypto_helpdesk/ticket/"
+                                  + number
+                                  + "/attachment/" + filename)
+        file.write(content)
+        file.close()
+        
     def ticketClose(self, ticketId):
         print(self.connection.post("/bin/crypto_helpdesk/ticket/" + ticketId
                                    + "/close", confirm=True))
@@ -73,3 +123,19 @@ class Helper:
     def ticketTitle(self, ticketId):
         print(self.connection.get("/bin/crypto_helpdesk/" + ticketId
                                   + "/title"))
+
+    def keyManagerGetInfo(self, user):
+        print(self.connection.get("/bin/key-management/" + user))
+
+    def keyManagerDownloadPublicKey(self, user):
+        public_key = self.connection.get("/bin/key-management/" + user + "/pk")
+        file = open('public-key', 'w')
+        file.write(public_key)
+        file.close()
+
+    def keyManagerUploadPublicKey(self, pk):
+        print(self.connection.post("/bin/key-management/upload-pk", public_key = pk))
+
+    def keyManagerUploadPublicKeyForce(self, pk):
+        print(self.connection.post("/bin/key-management/upload-pk", public_key = pk, confirm = True))
+
